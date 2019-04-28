40491d8c23fe43201afbe47481a673a67b0a9e96
Sunjay Varma <varma.sunjay@gmail.com>
2019-04-28 10:31:16 -0400

    Cleaning up code slightly

--- src/bin/kale.rs
+++ src/bin/kale.rs
@@ -9,7 +9,10 @@ use minifb::{Window, WindowOptions, Key, KeyRepeat};
 use vek::{Vec3, Vec4, Mat4};
 
 struct Shader<'a> {
+    /// The vertices to render
     vertices: &'a [FillVertex],
+    /// The Model-View-Projection matrix
+    mvp: Mat4<f32>,
 }
 
 impl<'a> Pipeline for Shader<'a> {
@@ -21,12 +24,8 @@ impl<'a> Pipeline for Shader<'a> {
     fn vert(&self, &index: &Self::Vertex) -> ([f32; 3], Self::VsOut) {
         let vert = self.vertices[index as usize];
         let pos = Vec4::from_point(vert.position.to_3d().to_array());
-        let mvp = Mat4::rotation_x(2.0*PI/2.0) * Mat4::scaling_3d(0.5);
-        // let mvp = Mat4::<f32>::identity();
-        let pos_cam = Vec3::from(mvp * pos).into_array();
-        let normal = mvp * Vec4::from_point(vert.normal.to_3d().to_array());
-        // let normal = Vec3::from(normal.normalized());
-        dbg!((pos_cam, ()))
+        let pos_cam = Vec3::from(self.mvp * pos).into_array();
+        (pos_cam, ())
     }
 
     #[inline(always)]
@@ -62,7 +61,7 @@ fn main() {
         // Compute the tessellation.
         let result = tessellator.tessellate_path(
             path.iter(),
-            &FillOptions::default(),
+            &FillOptions::default().with_normals(false),
             &mut vertex_builder
         );
         assert!(result.is_ok());
@@ -77,8 +76,12 @@ fn main() {
     let mut color = Buffer2d::new([WIDTH, HEIGHT], 0);
     let mut depth = Buffer2d::new([WIDTH, HEIGHT], 1.0);
 
+    // Need to rotate by 180 degrees because the tesselator flips all the vertices
+    // Scaling needs to be set such that all coordinates are between -1.0 and 1.0
+    let mvp = Mat4::rotation_x(PI) * Mat4::scaling_3d(0.5);
     let shader = Shader {
         vertices: &buffers.vertices[..],
+        mvp,
     };
     shader.draw::<rasterizer::Triangles<_>, _>(
         &buffers.indices[..],
